4a4b1cafb0ce03e369dd295cc12ce903f5db342e
Dana Greene <danagreene29@gmail.com>
2019-08-13 15:50:14 -0400

    Next we have to figure out how to save user information.

--- fia.js
+++ fia.js
@@ -1,1 +1,11 @@
 
+
+// function othername() {
+//     var input = document.getElementById("userInput").value;
+//     alert(input);
+// }
+//
+// function n1() {
+//   var input= document.getElementById("name1").value;
+//   alert(input);
+}
