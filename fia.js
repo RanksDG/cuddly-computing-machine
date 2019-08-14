576f5ae1049c0d82939036a5a90ea41aa937b69d
Dana Greene <danagreene29@gmail.com>
2019-08-14 15:59:55 -0400

    Fixed some problems.

--- fia.js
+++ fia.js
@@ -1,11 +1,13 @@
-
-
+console.log(sdfghjk);
 // function othername() {
 //     var input = document.getElementById("userInput").value;
 //     alert(input);
-// }
-//
-// function n1() {
-//   var input= document.getElementById("name1").value;
-//   alert(input);
+}
+
+function n1() {
+  var input = document.getElementById("name1").value;
+   alert(input);
+   if (input!= null) {
+    document.getElementById("name1")= " Sorry to hear that" + input+". Remember when you did this?"
+   }
 }
---
+++ script.js
@@ -0,0 +1,76 @@
+
+console.log("HELLO WORLD");
+
+
+function out1() {
+  // console.log('hi')
+  // var user, ach,passTime,prod,ani,secrt,carer,travl,drem,acc,lck,y;
+  var info = document.getElementById("formpadding");
+
+
+  if (info != ""){
+
+
+  // info = document.getElementById("formpadding");
+  y = info.elements["name"].value;
+  console.log(info)
+  console.log(y)
+  document.getElementById("demo").innerHTML="Hello "+y+" <br>";
+  document.getElementById("demo").innerHTML+="Welcome to FiaFia <br>";
+  // document.write("Hello" +y+ "Welcome to FiaFia");
+}
+
+// save survey answers in the function onclick from submit button, have the function return those values so that they are saved in the js file and can then be accessed on other html pages
+}
+
+function test(){
+  document.getElementById('achieve').innerHTML = document.getElementById("formpadding").elements["achieve"].value
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  //
+  // function output1() {
+  //   var user,info, ach,passTime,prod,ani,secrt,carer,travl,drem,acc,lck,y;
+  //   info = document.getElementById("formpadding");
+  //   y = info.elements["name"].value;
+  //   document.getElementById("demo").innerHTML="Hello" +y+ "Welcome to FiaFia""
+  //   documentwrite("Hello" +y+ "Welcome to FiaFia");
+  //
+  //   <p id="demo"></p>
+  //
+  //
+  //
+  //
+  //
+  //
+  //
+  //   user = document.getElementById("name1");
+  //   ach = document.getElementById("achievement");
+  //   passTime = document.getElementById("fun");
+  //   prod = getElementById("proud");
+  //   ani =
+  //   document.getElementById("animal");
+  //   secrt = document.getElementById("secret");
+  //   carer = document.getElementById("career");
+  //   travl = document.getElementById("travel");
+  //   drem = document.getElementById("dream");
+  //   accom = document.getElementById("accom");
+  //   lck = document.getElementById("lack");
+  //
+  //
+  // }
